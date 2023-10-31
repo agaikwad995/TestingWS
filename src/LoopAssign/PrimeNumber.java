3534b6e0a174b49d574931dc659da78b74c36fb8
gaikw <gaikw@Akash.mshome.net>
2023-10-31 20:01:59 +0530

    Updated the code instead of int j. used flag as boolean

--- src/LoopAssign/PrimeNumber.java
+++ src/LoopAssign/PrimeNumber.java
@@ -7,16 +7,17 @@ public static void main(String[] args) {
 	Scanner scan= new Scanner(System.in);
 	System.out.println("Enter a number");
 	int number = scan.nextInt();
-	int i , j = 0; 
-	
+	int i; 
+	boolean flag = false;
 	for (i = 2; i <= number/2; i++ ) {
 		if (number%i==0)
 		{
-			j++;
+			flag = true;
+			break;
 		}
 	}
 	
-	if (j == 0 && number!= 1) {
+	if (!flag) {
 		System.out.println("Prime number");
 		}
 	else
